6e372cacfba6b9b2b61fb5b91ca2b3da455106d2
Nishadi Gunasinghe <nishadig816@gmail.com>
2025-09-19 21:31:46 +0100

    Created basic project structure

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main className="pt-20">
+        <h1 className="text-3xl font-bold text-center mt-10">
+          Welcome to My Website 🚀
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
